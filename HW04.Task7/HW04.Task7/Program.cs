b4d81eab7f9dd2965e1210ba4254145a304d38a0
Andrey Ryshkevich <paulbegitson@mail.ru>
2020-11-10 01:20:04 +0300

    Не смог понять задачу

---
+++ HW04.Task7/HW04.Task7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW04.Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = Convert.ToInt32(Console.ReadLine());
+            int W 
+            int S = Convert.ToInt32(Console.ReadLine());
+            int A = Convert.ToInt32(Console.ReadLine());
+            int D = Convert.ToInt32(Console.ReadLine());
+
+            if (i == W)
+            {
+                Console.WriteLine("Right!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+        }
+    }
+}
