449a97d9fe4a26cce027ff3e3aed562bc2bf2182
Andrey Ryshkevich <paulbegitson@mail.ru>
2020-11-17 13:27:08 +0300

    First task

---
+++ hm1/hm1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace hm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random();
+            Numenter();
+            SumNumbers();
+
+            static void Random()
+            {
+            int[] rannumb = new int[5];
+            Random rand = new Random();
+            for (int i = 0; i < rannumb.Length; i++)
+            {
+                rannumb[i] = rand.Next();
+                Console.WriteLine(rannumb[i]);
+            }
+            Console.ReadLine();
+            }
+        }
+        static void Numenter()
+        {
+            int[] enter = new int[5];
+            for (int i = 0; i < enter.Length; i++)
+            {
+                Console.WriteLine(i);
+                enter[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void SumNumbers()
+        {
+            int[] array1 = new int[] {1, 2, 3, 4, 5};
+            int sum = array1[0] + array1[1];
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("Sum:" + sum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
