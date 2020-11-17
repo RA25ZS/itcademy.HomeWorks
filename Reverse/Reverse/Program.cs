4c25e55bb9bb24a533cb919a82f0c9487af1881b
Andrey Ryshkevich <paulbegitson@mail.ru>
2020-11-17 14:57:17 +0300

    Task 3 (Reverse)

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "!yad taerg evaH";
+            char[] charArray = message.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char messageChar in charArray)
+            {
+                Console.Write(messageChar);
+            }
+            Console.ReadLine();
+        }
+    }
+}
