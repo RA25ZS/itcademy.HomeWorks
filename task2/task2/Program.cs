5ad5737215f4931e23405159af50a274786e7870
Andrey Ryshkevich <paulbegitson@mail.ru>
2020-11-17 14:48:30 +0300

    Task 2

---
+++ task2/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 3; i == 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
