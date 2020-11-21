6625e3c70503e30a92bafd9826eb991341f64067
Andrey Ryshkevich <paulbegitson@mail.ru>
2020-11-21 16:27:17 +0300

    Task 1 HM07

---
+++ HM07/HM07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HM07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter: ");
+            string phrase = Console.ReadLine();
+            phrase = phrase.Replace("o", "a");
+            string[] words = phrase.Split(' ');
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word};");
+            }
+            Console.ReadLine();
+        }
+    }
+}
