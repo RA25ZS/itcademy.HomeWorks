bb5d7072e802036bd65d9717de2748a2f00795d0
Andrey Ryshkevich <paulbegitson@mail.ru>
2020-12-01 20:03:52 +0300

    hm_09

---
+++ HW_09/HW_09/Motorcycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssemblyOne
+{
+    class Motorcycle
+    {
+        const ushort MaxSpeed = 300;
+        string vinNumber = "2C777841-69BB-4647-A3C7-7CDB8521BB16";
+        int odometr = 20_000;
+
+        void StartEngine()
+        {
+
+        }
+    }
+}
